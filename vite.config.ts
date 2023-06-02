8530a6e47c0c9e70e90ac296df551cc0abd84d93
nk <nk-noreply@gmail.com>
2023-06-02 15:25:49 +0800

    chore: add rollup plugin visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(), visualizer() as PluginOption],
+  plugins: [
+    react(),
+    visualizer({
+      brotliSize: true,
+      gzipSize: true,
+      filename: '.visual/stats.html',
+      open: true,
+    }) as PluginOption,
+  ],
 });
